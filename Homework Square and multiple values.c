56a0309c2ca505bb61562a5f4eb3361ab4a69691
AdriaPuerto04 <adria.puerto.perez@estudiantat.upc.edu>
2024-10-15 17:51:54 +0200

    Update Homework Square and multiple values.c

--- Homework Square and multiple values.c
+++ Homework Square and multiple values.c
@@ -33,3 +33,58 @@ int main()
 
 	return 0;
 }
+
+//----------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------
+
+
+#include <stdio.h>
+
+void FirstAndLastMultipleInRange(int val, int min, int max) {
+	if (val <= max && val >= min){
+		for (int num = min; num < max; num++) {
+
+			if (val % num == 0){
+
+			printf("%i", num);
+				if (num < val) {
+					printf(", ");
+
+				}
+				else if (num == val) {
+					printf(".");
+
+				}
+
+			}
+		
+		}
+	}
+	else {
+		printf("%i is not in the range!", val);
+	}
+	
+}
+
+
+
+int main() 
+{
+
+	int vorp, max, min;
+
+	printf("Type a number:\n");
+	scanf_s("%i", &vorp);
+
+	printf("Enter the MINIMUM number in the range:\n");
+	scanf_s("%i", &min);
+
+	printf("Enter the MAXIMUM number in the range:\n");
+	scanf_s("%i", &max);
+
+
+	FirstAndLastMultipleInRange(vorp, min, max);
+
+	return 0;
+}
